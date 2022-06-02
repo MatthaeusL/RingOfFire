f1ea6faaa27da48c122a07a0051bb872bb77dfe9
MatthaeusL <mat@lispage.com>
2022-06-02 09:43:15 +0200

    added game component + game ts

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation:boolean = false;
+  game: Game = new Game;
+  currentCard: any; 
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
+  pickCard(){
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      
+      console.log(this.currentCard);
+      console.table(this.game.playedCard)
+      
+      this.pickCardAnimation = true;
+      
+      setTimeout(() => {
+        this.game.playedCard.push(this.currentCard);
+        this.pickCardAnimation = false;
+  
+      }, 1000);
+    }
+  }
+}
